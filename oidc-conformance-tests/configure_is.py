8ca74f8d7be3ae64f9b6dc744b4818a95c90cf59
Dilshan Senarath <dilshan99news@gmail.com>
2024-02-21 10:36:39 +0530

    fix server start triggering issue

--- oidc-conformance-tests/configure_is.py
+++ oidc-conformance-tests/configure_is.py
@@ -343,7 +343,7 @@ def unpack_and_run(zip_file_name):
         process = subprocess.Popen("./" + dir_name + "/bin/wso2server.sh", stdout=subprocess.PIPE)
         while True:
             output = process.stdout.readline()
-            if b'..................................' in output:
+            if b'WSO2 Carbon started' in output:
                 print("\nServer Started")
                 break
             if output:
